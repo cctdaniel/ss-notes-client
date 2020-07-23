a8d3ed36814075862c9c3fd0e28e9ec09bb0e2cc
Daniel Chew <cctdaniel@outlook.com>
2020-07-23 20:35:58 +0800

    Build React App

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-2",
     BUCKET: "ss-notes",
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://04l2cm0pfh.execute-api.us-east-2.amazonaws.com/prod/",
+    URL: "https://04l2cm0pfh.execute-api.us-east-2.amazonaws.com/prod",
   },
   cognito: {
     REGION: "us-east-2",
@@ -13,4 +14,6 @@ export default {
     APP_CLIENT_ID: "e9puulge73rqhec576egr94of",
     IDENTITY_POOL_ID: "us-east-2:8ef5bafb-faad-4fd1-ad5e-be41b13fb51a",
   },
+  STRIPE_KEY:
+    "pk_test_51H7auQL3OBkQEBucVoxem7H1rG8JB0UapxdNI7lTBiRO1IL3LoFRWhKZHWNGbGssiQCGPWU8YnSzSSH0WONs5XqA00ZQj8dGeM",
 }
